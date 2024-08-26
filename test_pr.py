41162305c9ab23379555c1d7c03ea89fe3189dee
sai sundar <saisundar100@gmail.com>
2024-08-26 09:00:45 +0530

    Create test_pr.py

---
+++ test_pr.py
@@ -0,0 +1,6 @@
+Example.com
+
+export API_KEY="AK1234FAKE5898KEY91011DUMMY"
+export SECRET_KEY="test_51H7u3DAmMEGAFakeSecretKey"
+export PASSWORD="P@ssw0rd!Complex123"
+export ACCESS_TOKEN="ya29.A0Af6SMAkDummyAccessToken"
