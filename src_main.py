127fe92f7dbf561c98d47e036a86f04dc2ac4e9b
sai sundar <saisundar100@gmail.com>
2024-08-26 09:15:08 +0530

    Update src_main.py

--- src_main.py
+++ src_main.py
@@ -1,7 +1,7 @@
 example.com
 
 # Test file for scanning purposes
-password = "SecretPassword@123"
+  password = "SecretPassword@123"
 
 """
   Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
